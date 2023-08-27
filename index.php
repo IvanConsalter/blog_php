9944c6978c0cddee1486aa677e40dc96ff1f3126
Ivan Consalter <ivanconsalter1@gmail.com>
2023-08-27 15:58:26 -0300

    added templates

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
-
-  include_once("helpers/url.php")
+  include_once("templates/header.php")
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blog Codar</title>
-  <!-- Estilos -->
-  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
-
-  <!-- Fontes -->
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
-</head>
-<body>
-  <h1>Initial Config</h1>
-</body>
-</html>
+<?php
+  include_once("templates/footer.php")
+?>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+    <footer>
+      <p>Projeto Desenvolvido no Curso de <a href="https://www.udemy.com/course/php-do-zero-a-maestria-com-projetos-incriveis/">PHP na Udemy</a></p>
+    </footer>
+
+  </body>
+</main>
